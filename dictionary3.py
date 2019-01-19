d2827ed9302316b2520e2347488ff097ee1f4ba6
kaczmarekmichal <kaczmarekmichal@users.noreply.github.com>
2019-01-19 10:38:42 +0100

    new

--- dictionary3.py
+++ dictionary3.py
@@ -2,9 +2,9 @@ produkty = {'S122' : 'sukienka trojkat',
             'P123' : 'spodnie krata',
             'x212' : 'konsola' }
 
-igla ='x2x'
+igla ='x212'
 
 if igla in produkty:
     print("znalazlem {0}".format(igla))
 else:
-    print("Brak w magazynie {0}".format(igla))                
+    print("Brak w magazynie {0}".format(igla))
---
+++ new.py
@@ -0,0 +1,18 @@
+a = "ala ma kota"
+
+a="ala "+"ma " + "kota"
+
+print (len(a))
+
+print (a)
+
+x ="python"
+
+print (x[-4])
+
+print('{0}{1}{2}'.format('a','b','c'))
+
+z="ala"
+x="%s ma kota"%z
+
+print(x)
